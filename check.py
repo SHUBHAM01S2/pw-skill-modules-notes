8bc37a729a3f6e838ba6b4c2bde8edab81547883
Shubham Sharma <shubhamsharma9805083537@gmail.com>
2025-03-10 07:34:38 +0530

    add files

---
+++ check.py
@@ -0,0 +1,23 @@
+"""1st Program"""
+
+# account_balance = 5000
+# withdraw_amount = int(input("Enter the amount "))
+
+# if withdraw_amount <= account_balance:
+#     print("Transaction Successful ")
+#     account_balance = account_balance - withdraw_amount
+#     print("Remaining Account is: ",account_balance)
+# else:
+#     print('Insufficient funds. Transaction cancelled')
+
+"""2nd Program"""
+
+# age = int(input("Enter your age"))
+# if age <=5:
+#     print('Ticket is Free')
+# elif age <= 18:
+#     print('Ticket is of 100')
+# elif age <= 68:
+#     print('Ticket is of 150')
+# else:
+#     print('Ticket is of 100')
